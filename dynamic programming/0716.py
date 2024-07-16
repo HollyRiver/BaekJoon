b43f6676a96e23f39825e5df20c158224b24ed52
HollyRiver <hcssk2800@gmail.com>
2024-07-16 21:55:03 +0900

    .

---
+++ dynamic programming/0716.py
@@ -0,0 +1,31 @@
+      
+from sys import stdin
+
+T = int(stdin.readline().rstrip())
+
+lst = [[0]*14 for _ in range(15)]
+lst[0] = [i for i in range(1, 15)]
+
+for i in range(1, 15) :
+    lst[i][0] = 1
+    for j in range(1, 14) :
+        lst[i][j] = lst[i][j-1] + lst[i-1][j]
+
+for _ in range(T) :
+    k = int(stdin.readline().rstrip())
+    n = int(stdin.readline().rstrip())
+    
+    print(lst[k][n-1])
+    
+##-----------------------------------
+
+A, B, V = map(int, input().split())
+
+dpd = A-B
+
+if dpd*((V-B)//dpd)+A == V :
+    print((V-B)//dpd + 1)
+elif dpd*((V-B)//dpd)+A > V :
+    print((V-B)//dpd)
+else :
+    print((V-B)//dpd + 2)
